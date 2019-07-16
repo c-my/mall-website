a2928ff26cb75c4d1288f82c3b5a6de7f63302f5
c-my <cmy1113@outlook.com>
2019-07-16 10:13:08 +0800

    move order form class

---
+++ src/main/java/edu/neu/neumall/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package edu.neu.neumall.repository;
+
+import edu.neu.neumall.entity.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long> {
+}
