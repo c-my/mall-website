c2b4af5d14cfb41866ed6bb58ad2cda8fe0304f4
c-my <cmy1113@outlook.com>
2019-07-26 14:54:09 +0800

    order result

--- src/main/java/edu/neu/neumall/repository/ShippingAddrRepository.java
+++ src/main/java/edu/neu/neumall/repository/ShippingAddrRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Set;
 
 public interface ShippingAddrRepository extends JpaRepository<ShippingAddr, Integer> {
-    Set<ShippingAddr> findByOwner_ID(long userID);
+    Set<ShippingAddr> findByOwner_IDOrderByID(long userID);
 }
