3f232f8b48b1eda11b9c01522aecbd20cc495743
c-my <cmy1113@outlook.com>
2019-07-26 14:52:37 +0800

    change static image dir

--- src/main/java/edu/neu/neumall/configure/WebMvcConfig.java
+++ src/main/java/edu/neu/neumall/configure/WebMvcConfig.java
@@ -4,7 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 //@EnableWebMvc
@@ -17,6 +22,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedOrigins("http://localhost:8080");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        var currentPath = Paths.get(System.getProperty("user.dir"));
+        var staticPath = Paths.get(currentPath.toString(), "static");
+        var location = Paths.get(staticPath.toString(), "img");
+        location = Path.of(location.toString(), "/");
+        System.out.println(location);
+        registry.addResourceHandler("/**").addResourceLocations("file:///" + location.toString());
     }
 
     @Bean
