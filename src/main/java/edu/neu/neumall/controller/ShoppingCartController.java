fd5ec8a20a5b910476d79c0646ef3336785987fe
lihang19980418 <30917713+lihang19980418@users.noreply.github.com>
2019-09-03 16:50:59 +0800

    1

--- src/main/java/edu/neu/neumall/controller/ShoppingCartController.java
+++ src/main/java/edu/neu/neumall/controller/ShoppingCartController.java
@@ -1,6 +1,8 @@
 package edu.neu.neumall.controller;
 
 import edu.neu.neumall.entity.User;
+import edu.neu.neumall.repository.ShoppingCartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/cart")
 public class ShoppingCartController {
+    private ShoppingCartRepository shoppingCartRepository;
+
+    @Autowired
+    public ShoppingCartController(ShoppingCartRepository shoppingCartRepository){
+        this.shoppingCartRepository = shoppingCartRepository;
+    }
+
     @GetMapping
     public String shoppingCartPage(@AuthenticationPrincipal User user, Model model) {
-        model.addAttribute("cart", user.getShoppingCart());
-        return "";
+//        model.addAttribute("cart", user.getShoppingCart());
+        model.addAttribute("cart", shoppingCartRepository.findAll());
+        return "car.html";
     }
 }
